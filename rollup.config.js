552e2bd0efd3cd1ef35eba2a557ae214ec942933
Richard Scarrott <riscarrott@googlemail.com>
2021-06-20 17:09:51 +0100

    Generate TS types

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
   {
-    input: 'src/ok-computer.js',
-    output: {
-      name: 'ok-computer',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [typescript()]
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
-  {
-    input: 'src/ok-computer.js',
-    plugins: [typescript()],
+    input: 'src/ok-computer.ts',
+    plugins: [typescript(), commonjs(), resolve()],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
