f5729c51eace0b0779c00ad91e49605e1b07d51d
Richard Scarrott <riscarrott@googlemail.com>
2022-09-14 22:43:00 +0100

    Add PURE annotations

--- src/ok-computer.ts
+++ src/ok-computer.ts
@@ -303,7 +303,11 @@ export const max = (num: number) =>
     `Expected max ${num}`
   );
 
-export const nullish = err(or(undef, nul), 'Expected nullish');
+export const nullish = err(
+  /*@__PURE__*/
+  or(undef, nul),
+  'Expected nullish'
+);
 
 export const includes = (value: any) =>
   err(
@@ -678,6 +682,7 @@ export const match = (key: string) =>
   );
 
 export const email = err(
+  /*@__PURE__*/
   pattern(
     /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i
   ),
@@ -733,7 +738,9 @@ export const oxorPeer = (key: string) => oxorPeers(key);
 //   foo.id.startsWith('bar');
 // };
 // It allows ok computers' `assert` fn to handle the common assert case provided by node et al.
-export const exists = not<{}, ExtractErr<typeof nullish>>(nullish);
+export const exists = /*@__PURE__*/ not<{}, ExtractErr<typeof nullish>>(
+  nullish
+);
 
 export const truthy = create<{}>((value) => !!value)('Expected truthy value');
 
